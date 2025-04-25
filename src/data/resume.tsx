5fddb76cca0162abf9e5d27a1c36bc664ec2db65
joydeep-git <joydeep.accounts@tuta.io>
2025-04-25 21:08:09 +0530

    displaying resume and removed image display

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -8,7 +8,7 @@ export const DATA = {
   location: "Kolkata, WB",
   locationLink: "https://www.google.com/maps/place/kolkata",
   description:
-    "Full Stack Developer. I love building things and helping people.",
+    "Full Stack Developer",
   summary:
     "I started as a frontend developer specializing in Next.js, TypeScript and Redux. Over time, I expanded into backend development with Node.js, Express, and MongoDB. Now, I focus on building scalable full-stack applications and SaaS projects.",
   avatarUrl: "/me.png",
@@ -62,9 +62,9 @@ export const DATA = {
       },
       Resume: {
         name: "Resume",
-        url: "/cv.pdf",
+        url: "/JOYDEEP_DAS_RESUME.pdf",
         icon: Icons.resume,
-        navbar: false
+        navbar: true
       },
       Youtube: {
         name: "Youtube",
