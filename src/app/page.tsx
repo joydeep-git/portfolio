321c2d829c2e6518ba6427eb40874cd3b3526cef
joydeep-git <joydeep.accounts@tuta.io>
2025-05-15 18:35:32 +0530

    changed skills badge styles and removed linked from education

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Page() {
             >
               <ResumeCard
                 key={education.school}
-{/*                 href={education.href} */}
+                href={education.href}
                 logoUrl={education.logoUrl}
                 altText={education.school}
                 title={education.school}
@@ -105,7 +105,7 @@ export default function Page() {
           <div className="flex flex-wrap gap-1">
             {DATA.skills.map((skill, id) => (
               <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
-                <Badge key={skill}>{skill}</Badge>
+                <Badge className="p-1.5 text-base m-0.5 md:m-2" key={skill}>{skill}</Badge>
               </BlurFade>
             ))}
           </div>
