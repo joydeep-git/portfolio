86677113f0fa9751c9d831fc41b488a5641ecd5f
Joydeep Das <joydeep.accounts@tuta.io>
2025-05-15 16:07:53 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Page() {
             >
               <ResumeCard
                 key={education.school}
-                href={education.href}
+{/*                 href={education.href} */}
                 logoUrl={education.logoUrl}
                 altText={education.school}
                 title={education.school}
@@ -119,14 +119,9 @@ export default function Page() {
                 <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
                   My Projects
                 </div>
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Check out my latest work
-                </h2>
-                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
-                  favorites.
-                </p>
+{/*                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Check out my projects
+                </h2> */}
               </div>
             </div>
           </BlurFade>
